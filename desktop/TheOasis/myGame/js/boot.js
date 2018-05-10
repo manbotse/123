3783ef5da2caae36788d50e79ca4a138d440d1d1
Wenbo Xie <wxie10@ucsc.edu>
2018-05-09 18:49:40 -0700

    delete Desktop

--- desktop/TheOasis/myGame/js/boot.js
+++
@@ -1,9 +0,0 @@
-var bootState = {
-	create: function(){
-		//enable game physics
-		game.physics.startSystem(Phaser.Physics.ARCADE);
-        
-        //state changing
-		game.state.start('load');
-	}
-};
